81760e5b33158325454f35b71d7768069c0f72f3
Kalyayo7 <kalyayo7@gmail.com>
2024-12-28 21:33:24 +0300

    Create Chel_create.py

---
+++ Chel_create.py
@@ -0,0 +1,65 @@
+import file_operations
+from faker import Faker
+import random
+
+skill = ['Стремительный прыжок', 'Электрический выстрел', 'Ледяной удар', 'Стремительный удар', 'Кислотный взгляд', 'Тайный побег', 'Ледяной выстрел', 'Огненный заряд']
+
+replace_values = {'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+ 'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+ 'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+ 'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+ 'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+ 'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+ 'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+ 'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+ 'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+ 'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+ 'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+ 'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+ 'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+ 'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+ 'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+ 'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+ 'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+ 'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+ 'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+ 'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+ 'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+ 'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋'}
+
+fake = Faker("ru_RU")
+
+def rune_skills():
+    for i in range(len(skill)):
+        for x, y in replace_values.items():
+            skill[i]=skill[i].replace(x,y)
+
+
+rune_skills()
+
+
+def main():
+    for i in range(10):
+
+        skill_3 = random.sample(skill,3)
+
+        context = {
+          "first_name": fake.name(),
+          "last_name": '',
+          "job": fake.job(),
+          "town": fake.city(),
+          "strength": random.randint(3, 18),
+          "agility": random.randint(3, 18),
+          "endurance": random.randint(3, 18),
+          "intelligence": random.randint(3, 18),
+          "luck": random.randint(3, 18),
+          "skill_1": skill_3[0],
+          "skill_2": skill_3[1],
+          "skill_3": skill_3[2]
+         }
+
+        file_operations.render_template("charsheet.svg", f"gde_papka/{i}.svg", context)
+
+
+main()
+
